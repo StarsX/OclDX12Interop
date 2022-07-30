2cf056620ca267827ca68037dbc002148c02b08e
Tianchen Xu <TianchenX@outlook.com>
2022-07-30 21:57:18 +0800

    Update XUSG D3D12 headers and include local headers

--- OclDX12Interop/stdafx.h
+++ OclDX12Interop/stdafx.h
@@ -23,11 +23,11 @@
 
 #include <windows.h>
 
+#include "d3dx12.h"
 #include <dxgi1_5.h>
 #include <d3d11_1.h>
 #include <D3Dcompiler.h>
 #include <DirectXMath.h>
-#include "d3dx12.h"
 
 // OpenCL
 #undef min
